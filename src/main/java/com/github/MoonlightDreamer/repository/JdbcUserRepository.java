30a493b92ce6396a1c55fd6ac93ce73553edb7b9
Sadrutdin Zaynukov <SAZaynukov@ya.ru>
2022-11-01 18:39:17 +0300

    ДЗ - исправления на уроке

--- src/main/java/com/github/MoonlightDreamer/repository/JdbcUserRepository.java
+++ src/main/java/com/github/MoonlightDreamer/repository/JdbcUserRepository.java
@@ -3,42 +3,65 @@ package com.github.MoonlightDreamer.repository;
 import com.github.MoonlightDreamer.model.User;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.Objects;
+import java.util.Optional;
 
-public class JdbcUserRepository implements UserRepository{
+public class JdbcUserRepository implements UserRepository {
+    private final Connection connection;
 
-    private Statement statement;
-
-    public JdbcUserRepository(Statement statement) {
-        this.statement = statement;
+    public JdbcUserRepository(Connection connection) {
+        this.connection = Objects.requireNonNull(connection);
     }
 
     @Override
-    public User get(Integer id) throws SQLException {
-        ResultSet rs = statement.executeQuery("SELECT * FROM users WHERE id=" + id);
-        if(rs.next()) {
-            User user = new User();
-            user.setId(rs.getInt("id"));
-            user.setName(rs.getString("name"));
-            return user;
-        } else {
-            return null;
+    public Optional<User> get(Integer id) throws SQLException {
+        try (PreparedStatement ps = connection.prepareStatement("SELECT id, name FROM users WHERE id=?")) {
+            ps.setInt(1, id);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    User user = new User();
+                    user.setId(rs.getInt("id"));
+                    user.setName(rs.getString("name"));
+                    return Optional.of(user);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        return Optional.empty();
     }
 
     @Override
     public boolean delete(Integer id) throws SQLException {
-        statement.executeUpdate("DELETE FROM users WHERE id=" + id);
-        return get(id) == null;
+        try (PreparedStatement ps = connection.prepareStatement("DELETE FROM users WHERE id=?")) {
+            ps.setInt(1, id);
+            return ps.executeUpdate() == 1;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
     @Override
     public boolean create(User user) throws SQLException {
-        if(user.isNew()) {
-            statement.executeUpdate("INSERT INTO users(name) VALUES('" + user.getName() + "')");
+        if (!user.isNew()) {
+            return false;
         }
-        return user.isNew();
+        try (PreparedStatement ps = connection.prepareStatement("INSERT INTO users(name) VALUES(?) RETURNING id")) {
+            ps.setString(1, user.getName());
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                int id = rs.getInt(1);
+                user.setId(id);
+                return true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
--- src/main/java/com/github/MoonlightDreamer/repository/UserRepository.java
+++ src/main/java/com/github/MoonlightDreamer/repository/UserRepository.java
@@ -3,10 +3,11 @@ package com.github.MoonlightDreamer.repository;
 import com.github.MoonlightDreamer.model.User;
 
 import java.sql.SQLException;
+import java.util.Optional;
 
 public interface UserRepository {
 
-    User get(Integer id) throws SQLException;
+    Optional<User> get(Integer id) throws SQLException;
 
     boolean delete(Integer id) throws SQLException;
 
