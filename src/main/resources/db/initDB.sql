e54e7443b1fdaa16c06d08d991426509e1bc5739
Sadrutdin Zaynukov <SAZaynukov@ya.ru>
2022-11-01 13:47:50 +0300

    Почистил репозиторий

--- src/main/resources/db/initDB.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS users;
-DROP SEQUENCE IF EXISTS global_seq;
-
-CREATE SEQUENCE global_seq START WITH 100000;
-
-CREATE TABLE users (
-    id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
-    name        VARCHAR                             NOT NULL
-);
--- src/main/resources/db/populateDB.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO users (name)
-VALUES ('User1'),
-       ('Admin');
